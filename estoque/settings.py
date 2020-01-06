4d90804575ff2c59e09eedf67a4f3c37b5614552
victorpajeu <pajeuvictor@gmail.com>
2020-01-06 14:14:09 -0300

    Criação da app Estoque. close #6

--- estoque/settings.py
+++ estoque/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'decouple',
     'core',
     'produto',
+    'estoque_',
 ]
 
 MIDDLEWARE = [
---
+++ estoque_/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.db import models
+from core.models import TimeStampedModel
+from produto.models import Produto
+
+
+MOVIMENTO=(
+    ('e', 'entrada'),
+    ('s', 'saida'),
+)
+
+
+class Estoque(TimeStampedModel):
+    funcionario = models.ForeignKey(User, on_delete=models.CASCADE)
+    nf = models.PositiveIntegerField('nota fiscal', null=True, blank=True),
+    movimento = models.CharField(max_length=1, choices=MOVIMENTO),
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return str(self.pk)
+
+
+class EstoqueItens(models.Model):
+    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE),
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE),
+    quantidade = models.PositiveIntegerField(),
+    saldo = models.PositiveIntegerField(),
+
+    def __str__(self):
+        return f'{self.pk}''{self.estoque}'-'{self.produto}'
