9f205b612344b7d4fbc9d556dcfd501dd04c46ec
victorpajeu <pajeuvictor@gmail.com>
2020-01-06 10:45:00 -0300

    Criação da app Core. close #4

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(
+        'Criado em',
+        auto_now_add=True,
+        auto_now=False,
+    )
+    modified = models.DateTimeField(
+        'modificado em',
+        auto_now_add=False,
+        auto_now=True,
+    )
